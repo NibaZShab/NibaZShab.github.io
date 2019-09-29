89e4efb0dacf2a8e9228e3a513e44dff497035d0
NibaZShab <nibazshab@gmail.com>
2019-09-29 18:40:58 +0800

    Site updated: 2019-09-29 18:40:56

--- 404/when/install.sh
+++ 404/when/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pkg install -y wget
-wget -O $PREFIX/bin/when https://github.com/NibaZShab/NibaZShab.github.io/raw/master/404/when/when.sh
+wget -O $PREFIX/bin/when https://nibazshab.github.io/404/when/when.sh
 chmod 777 $PREFIX/bin/when
 echo "进度 [100%]\n输入 when 启动本程序"
 exit
