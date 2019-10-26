6aaf78c2277e29e35b70b2258b6019fd11d814bb
NibaZShab <nibazshab@gmail.com>
2019-10-27 07:53:57 +0800

    Site updated: 2019-10-27 07:53:55

--- 404/when/when.sh
+++ 404/when/when.sh
@@ -30,7 +30,7 @@ sleep 0.04s
 echo -e " $colorend"
 }
 function home0(){
-cd $HOME
+cd $home1
 echo -e "
 \n
 01)  好van的东西
@@ -48,28 +48,28 @@ echo -e "
 ---------------------------------------------
 "
 }
-function home(){
-read -p "[when]# 你选择的序号是：" jia
-case $jia in
+function home1(){
+read -p "[when]# 你选择的序号是：" home
+case $home in
 01)
 	clear
 	logo
 	sleep 1s
 	game0
-	game
+	game1
 	;;
 02)
 	sleep 0.5s
-	if test -e $HOME/.termux
+	if test -e $home1/.termux
 	then
 		:
 	else
-		mkdir -p $HOME/.termux
+		mkdir -p $home1/.termux
 	fi
-	echo -e "extra-keys = [['$','>','-','\"','~','/','*'],['ESC','(','HOME','UP','END',')','PGUP'],['CTRL','[','LEFT','DOWN','RIGHT',']','PGDN']]" > $HOME/.termux/termux.properties
+	echo -e "extra-keys = [['$','>','-','\"','~','/','*'],['ESC','(','home1','UP','END',')','PGUP'],['CTRL','[','LEFT','DOWN','RIGHT',']','PGDN']]" > $home1/.termux/termux.properties
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 请重启 termux $colorend"
 	home0
-	home
+	home1
 	;;
 03)
 	sleep 0.5s
@@ -79,7 +79,7 @@ case $jia in
 	apt update && apt upgrade
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 请重启 termux $colorend"
 	home0
-	home
+	home1
 	;;
 04)
 	sleep 0.5s
@@ -87,7 +87,7 @@ case $jia in
 	pkg in atilo-cn
 	echo -e "$colorhint 进度 [100%] $colorend"
 	home0
-	home
+	home1
 	;;
 05)
 	sleep 0.5s
@@ -96,23 +96,23 @@ case $jia in
 	pkg in -y nodejs-lts
 	pkg in -y openssh
 	pkg in -y wget
-	rm -rf $HOME/博客
-	mkdir -p $HOME/博客
-	wget -O $HOME/博客/02.zip https://github.com/NibaZShab/NibaZShab.github.io/releases/download/02/02.zip
-	cd $HOME/博客
-	unzip $HOME/博客/02.zip
-	rm -rf $HOME/博客/02.zip
+	rm -rf $home1/博客
+	mkdir -p $home1/博客
+	wget -O $home1/博客/02.zip https://github.com/NibaZShab/NibaZShab.github.io/releases/download/02/02.zip
+	cd $home1/博客
+	unzip $home1/博客/02.zip
+	rm -rf $home1/博客/02.zip
 	npm install -g hexo-cli
 	npm install --save hexo
-	mkdir -p $HOME/博客/404
-	cd $HOME/博客/404
+	mkdir -p $home1/博客/404
+	cd $home1/博客/404
 	hexo init
-	rm -rf $HOME/博客/404/_config.yml
-	mv -f $HOME/博客/_config.yml $HOME/博客/404
-	mv -f $HOME/博客/up.sh $HOME/博客/404
-	chmod 777 $HOME/博客/404/up.sh
-	mv -f $HOME/博客/inside $HOME/博客/404/themes
-	cd $HOME/博客/404
+	rm -rf $home1/博客/404/_config.yml
+	mv -f $home1/博客/_config.yml $home1/博客/404
+	mv -f $home1/博客/up.sh $home1/博客/404
+	chmod 777 $home1/博客/404/up.sh
+	mv -f $home1/博客/inside $home1/博客/404/themes
+	cd $home1/博客/404
 	git config --global user.name "NibaZShab"
 	git config --global user.email "nibazshab@gmail.com"
 	git init
@@ -120,7 +120,7 @@ case $jia in
 	echo -e "\n $colorhint 接下来请一路回车即可 $colorend"
 	sleep 2s
 	ssh-keygen -t rsa -C "nibazshab@gmail.com"
-	cd $HOME/博客
+	cd $home1/博客
 	git clone https://github.com/NibaZShab/NibaZShab.github.io.git
 	if test -e /sdcard/$
 	then
@@ -128,49 +128,49 @@ case $jia in
 	else
 		mkdir -p /sdcard/$
 	fi
-	cd $HOME
-	ln -s /sdcard/$ $HOME/404
-	cd $HOME/博客/NibaZShab.github.io
-	mv -f $HOME/博客/NibaZShab.github.io/404 $HOME/404/$
-	rm -rf $HOME/博客/NibaZShab.github.io
-	mkdir -p $HOME/博客/404/source/about
-	mkdir -p $HOME/博客/404/source/links
-	rm -rf $HOME/博客/404/source/_posts/*
-	cp $HOME/404/$/markdown/page/* $HOME/博客/404/source/_posts
-	cp $HOME/404/$/markdown/about.md $HOME/博客/404/source/about/index.md
-	cp $HOME/404/$/markdown/links.md $HOME/博客/404/source/links/index.md
-	cp $HOME/404/$/markdown/book.md $HOME/博客/404
-	chmod 777 $HOME/博客/404/book.md
+	cd $home1
+	ln -s /sdcard/$ $home1/404
+	cd $home1/博客/NibaZShab.github.io
+	mv -f $home1/博客/NibaZShab.github.io/404 $home1/404/$
+	rm -rf $home1/博客/NibaZShab.github.io
+	mkdir -p $home1/博客/404/source/about
+	mkdir -p $home1/博客/404/source/links
+	rm -rf $home1/博客/404/source/_posts/*
+	cp $home1/404/$/markdown/page/* $home1/博客/404/source/_posts
+	cp $home1/404/$/markdown/about.md $home1/博客/404/source/about/index.md
+	cp $home1/404/$/markdown/links.md $home1/博客/404/source/links/index.md
+	cp $home1/404/$/markdown/book.md $home1/博客/404
+	chmod 777 $home1/博客/404/book.md
 	npm install --save hexo-deployer-git
-	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 读取ssh密钥请输入 cat $HOME/.ssh/id_rsa.pub $colorend \n $colorhint 检测ssh连接状况请输入 ssh -T git@github.com $colorend \n\n $colorhint 注: 此为作者个人的博客，部分文件和操作逻辑较为难以理解 $colorend \n $colorhint 建议前往 https://nibazshab.github.io/post/04 $colorend \n $colorhint 可按照小白教程搭建自己的博客 $colorend"
+	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 读取ssh密钥请输入 cat $home1/.ssh/id_rsa.pub $colorend \n $colorhint 检测ssh连接状况请输入 ssh -T git@github.com $colorend \n\n $colorhint 注: 此为作者个人的博客，部分文件和操作逻辑较为难以理解 $colorend \n $colorhint 建议前往 https://nibazshab.github.io/post/04 $colorend \n $colorhint 可按照小白教程搭建自己的博客 $colorend"
 	home0
-	home
+	home1
 	;;
 06)
 	sleep 0.5s
 	echo -e "\n\n\n\n                   Welcome to Termux!\n\n           I'm NibaZShab,thanks for use \"when\"\n\n\n\n                      代码使人头冷\n                    专业护发，用飘柔\n\n ------------------------------------------------------\n             民生各有所乐兮，余独好修以为常\n             虽体解吾犹未变兮，岂余心之可惩\n ------------------------------------------------------\n\n                    Hello  my  World\n\n\n" > $PREFIX/etc/motd
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 请重启 termux $colorend \n $colorhint 如显示出问题，请双指捏合屏幕调整终端大小 $colorend"
 	home0
-	home
+	home1
 	;;
 07)
 	sleep 0.5s
 	pkg in -y golang
 	pkg in -y git
-	cd $HOME
+	cd $home1
 	git clone https://github.com/iikira/BaiduPCS-Go.git
-	cd $HOME/BaiduPCS-Go
+	cd $home1/BaiduPCS-Go
 	echo -e "$colorhint 编译时间较长，请耐心等待 $colorend"
 	sleep 2s
 	GOOS=android GOARCH=arm64 go build -o bdy main.go
-	mv -f bdy $HOME
-	cd $HOME
-	rm -rf $HOME/BaiduPCS-Go
-	chmod -Rf 777 $HOME/go
-	rm -rf $HOME/go
+	mv -f bdy $home1
+	cd $home1
+	rm -rf $home1/BaiduPCS-Go
+	chmod -Rf 777 $home1/go
+	rm -rf $home1/go
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 输入 ~/bdy 开始 $colorend"
 	home0
-	home
+	home1
 	;;
 08)
 	sleep 0.5s
@@ -180,34 +180,34 @@ case $jia in
 	rm -rf /sdcard/Download/aria2
 	mkdir -p /sdcard/Download/aria2
 	touch /sdcard/Download/aria2/aria2.session
-	cd $HOME
+	cd $home1
 	wget -O $PREFIX/etc/aria2.conf https://github.com/NibaZShab/NibaZShab.github.io/releases/download/09/09.conf
-	rm -rf $HOME/aria2
-	echo -e "echo \"rpc-key: 123456\"\nsleep 2s\nam start -a android.intent.action.VIEW -d http://aria2.net\naria2c --conf-path=$PREFIX/etc/aria2.conf" >> $HOME/aria2
-	chmod 777 $HOME/aria2
+	rm -rf $home1/aria2
+	echo -e "echo \"rpc-key: 123456\"\nsleep 2s\nam start -a android.intent.action.VIEW -d http://aria2.net\naria2c --conf-path=$PREFIX/etc/aria2.conf" >> $home1/aria2
+	chmod 777 $home1/aria2
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 输入 ~/aria2 开始 $colorend"
 	home0
-	home
+	home1
 	;;
 09)
 	sleep 0.5s
 	pkg in -y wget
-	rm -rf $HOME/install.sh
+	rm -rf $home1/install.sh
 	wget https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh
 	echo -e "$colorhint \n推荐配色值 → 19 19 $colorend \n"
 	sleep 3s
-	sh $HOME/install.sh
+	sh $home1/install.sh
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 请重启 termux $colorend"
-	rm -rf $HOME/storage
+	rm -rf $home1/storage
 	home0
-	home
+	home1
 	;;
 10)
 	clear
 	logo
 	sleep 1s
 	work0
-	work
+	work1
 	;;
 11)
 	sleep 0.5s
@@ -225,7 +225,7 @@ case $jia in
 	echo "$wz" |curl -F-=\<- qrenco.de
 	sleep 4s
 	home0
-	home
+	home1
 	;;
 00)
 	clear
@@ -235,12 +235,12 @@ case $jia in
 *)
 	echo -e "[when]# 你正确输入了一个错误序号！"
 	sleep 1s
-	home
+	home1
 	;;
 esac
 }
 function game0(){
-cd $HOME
+cd $home1
 echo -e "
 \n
 01)  小火车
@@ -256,43 +256,43 @@ echo -e "
 ---------------------------------------------
 "
 }
-function game(){
-read -p "[when]# 你选择的序号是：" vande
-case $vande in
+function game1(){
+read -p "[when]# 你选择的序号是：" game
+case $game in
 01)
 	sleep 0.5s
 	pkg in -y sl
 	echo -e "$colorhint 进度[100%] $colorend \n $colorhint 输入 sl 开始 $colorend"
 	game0
-	game
+	game1
 	;;
 02)
 	sleep 0.5s
 	pkg in -y nyancat
 	echo -e "$colorhint 进度[100%] $colorend \n $colorhint 输入 nyancat 开始 $colorend"
 	game0
-	game
+	game1
 	;;
 03)
 	sleep 0.5s
 	pkg in -y screenfetch
 	echo -e "$colorhint 进度[100%] $colorend \n $colorhint 输入 screenfetch 开始 $colorend"
 	game0
-	game
+	game1
 	;;
 04)
 	sleep 0.5s
 	pkg in -y neofetch
 	echo -e "$colorhint 进度[100%] $colorend \n $colorhint 输入 neofetch 开始 $colorend"
 	game0
-	game
+	game1
 	;;
 05)
 	sleep 0.5s
 	pkg in -y cowsay
 	echo -e "$colorhint 进度[100%] $colorend \n $colorhint 输入 cowsay 要说的话 开始 $colorend"
 	game0
-	game
+	game1
 	;;
 06)
 	sleep 0.5s
@@ -316,35 +316,35 @@ case $vande in
 08)
 	sleep 0.5s
 	pkg in -y wget
-	wget -O $HOME/van https://nibazshab.github.io/404/when/van.sh
-	chmod 777 $HOME/van
+	wget -O $home1/van https://nibazshab.github.io/404/when/van.sh
+	chmod 777 $home1/van
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 输入 ~/van 开始 $colorend"
 	game0
-	game
+	game1
 	;;
 09)
 	sleep 0.5s
 	pkg in -y nsnake
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 输入 nsnake 开始 $colorend \n $colorhint 如无法运行，请双指捏合屏幕调整终端大小 $colorend"
 	game0
-	game
+	game1
 	;;
 00)
 	clear
 	logo
 	sleep 1s
 	home0
-	home
+	home1
 	;;
 *)
 	echo -e "[when]# 你正确输入了一个错误序号！"
 	sleep 1s
-	game
+	game1
 	;;
 esac
 }
 function work0(){
-cd $HOME
+cd $home1
 echo -e "
 \n
 01)  python
@@ -355,56 +355,56 @@ echo -e "
 ---------------------------------------------
 "
 }
-function work(){
-read -p "[when]# 你选择的序号是：" huanjing
-case $huanjing in
+function work1(){
+read -p "[when]# 你选择的序号是：" work
+case $work in
 01)
 	sleep 0.5s
 	pkg in -y python
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 输入 python 进入python环境 $colorend"
 	work0
-	work
+	work1
 	;;
 02)
 	sleep 0.5s
 	pkg in -y wget
-	wget -O $HOME/jdk.deb https://github.com/NibaZShab/NibaZShab.github.io/releases/download/08/08.deb
-	dpkg -i $HOME/jdk.deb
-	rm -rf $HOME/jdk.deb
+	wget -O $home1/jdk.deb https://github.com/NibaZShab/NibaZShab.github.io/releases/download/08/08.deb
+	dpkg -i $home1/jdk.deb
+	rm -rf $home1/jdk.deb
 	echo -e "$colorhint 进度 [100%] $colorend"
 	work0
-	work
+	work1
 	;;
 03)
 	sleep 0.5s
 	pkg in -y golang
 	echo -e "$colorhint 进度 [100%] $colorend"
 	work0
-	work
+	work1
 	;;
 04)
 	sleep 0.5s
 	pkg in -y clang
 	echo -e "$colorhint 进度 [100%] $colorend"
 	work0
-	work
+	work1
 	;;
 00)
 	clear
 	logo
 	sleep 1s
 	home0
-	home
+	home1
 	;;
 *)
 	echo -e "[when]# 你正确输入了一个错误序号！"
 	sleep 1s
-	work
+	work1
 	;;
 esac
 }
 logo
 sleep 1s
 home0
-home
+home1
 exit
