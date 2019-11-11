33773fbd76fc4016183271e069718f2b005b1fde
NibaZShab <nibazshab@gmail.com>
2019-11-11 23:32:45 +0800

    Site updated: 2019-11-11 23:32:44

--- 404/when/when.sh
+++ 404/when/when.sh
@@ -11,23 +11,23 @@ colorend="\033[0m"
 function logo(){
 echo -e "$colorlogo"
 echo -e "\n\n\n\n\n\n"
-sleep 0.04s
+sleep 0.025s
 echo -e "               +"
-sleep 0.04s
+sleep 0.025s
 echo -e "               +"
-sleep 0.04s
+sleep 0.025s
 echo -e "               +"
-sleep 0.04s
+sleep 0.025s
 echo -e "               +"
-sleep 0.04s
+sleep 0.025s
 echo -e "  +   +   +    +++++++    +++++++    +++++++"
-sleep 0.04s
+sleep 0.025s
 echo -e "  +   +   +    +     +    +     +    +     +"
-sleep 0.04s
+sleep 0.025s
 echo -e "  +   +   +    +     +    +++++++    +     +"
-sleep 0.04s
+sleep 0.025s
 echo -e "  +   +   +    +     +    +          +     +"
-sleep 0.04s
+sleep 0.025s
 echo -e "  +++++++++    +     +    +++++++    +     +"
 echo -e " $colorend"
 }
@@ -36,33 +36,33 @@ function home0(){
 cd $HOME
 echo -e "\n\n"
 echo -e "01)  好van的东西"
-sleep 0.04s
+sleep 0.025s
 echo -e "02)  修复底部小键盘"
-sleep 0.04s
+sleep 0.025s
 echo -e "03)  切换清华源(暂不可用)"
-sleep 0.04s
+sleep 0.025s
 echo -e "04)  使用atilo安装linux发行版"
-sleep 0.04s
+sleep 0.025s
 echo -e "05)  安装hexo博客(作者的库)"
-sleep 0.04s
+sleep 0.025s
 echo -e "06)  切换termux问候语"
-sleep 0.04s
+sleep 0.025s
 echo -e "07)  安装命令行版百度云"
-sleep 0.04s
-echo -e "08)  安装aria2或axel下载工具"
-sleep 0.04s
+sleep 0.025s
+echo -e "08)  安装aria2或axel或annie下载工具"
+sleep 0.025s
 echo -e "09)  美化终端(zsh)"
-sleep 0.04s
+sleep 0.025s
 echo -e "10)  配置各种编程语言环境"
-sleep 0.04s
+sleep 0.025s
 echo -e "11)  网址转二维码"
-sleep 0.04s
+sleep 0.025s
 echo -e "99)  充值成为svip用户"
-sleep 0.04s
+sleep 0.025s
 echo -e "00)  退出"
-sleep 0.04s
+sleep 0.025s
 echo -e "---------------------------------------------"
-sleep 0.04s
+sleep 0.025s
 echo -e ""
 }
 function home1(){
@@ -71,7 +71,7 @@ case $home in
 01)
 	echo -e "\n\n\n\n\n"
 	logo
-	sleep 0.04s
+	sleep 0.025s
 	game0 ; game1
 	;;
 02)
@@ -162,7 +162,7 @@ case $home in
 	;;
 06)
 	sleep 0.5s
-	echo -e "\n\n\n\n                   Welcome to Termux!\n\n           I'm NibaZShab,thanks for use \"when\"\n\n\n\n                      代码使人头冷\n                    专业护发，用飘柔\n\n ------------------------------------------------------\n             民生各有所乐兮，余独好修以为常\n             虽体解吾犹未变兮，岂余心之可惩\n ------------------------------------------------------\n\n                    Hello  my  World\n\n\n" > $PREFIX/etc/motd
+	echo -e "\n\n\n\n                   Welcome to Termux!\n\n           I'm NibaZShab,thanks for use \"when\"\n\n\n\n                      代码使人头冷\n                    专业护发，用飘柔\n\n  ----------------------------------------------------\n             民生各有所乐兮，余独好修以为常\n             虽体解吾犹未变兮，岂余心之可惩\n  ----------------------------------------------------\n\n                    Hello  my  World\n\n\n" > $PREFIX/etc/motd
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 请重启 termux $colorend \n $colorhint 如显示出问题，请双指捏合屏幕调整终端大小 $colorend"
 	home0 ; home1
 	;;
@@ -181,12 +181,12 @@ case $home in
 	rm -rf $HOME/BaiduPCS-Go/
 	chmod -Rf 777 $HOME/go/
 	rm -rf $HOME/go/
-	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 输入 ~/bdy 开始 $colorend"
+	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 编译指令感谢 coolapk@萌系生物研究员 提供 $colorend \n $colorhint 输入 ~/bdy 开始 $colorend"
 	home0 ; home1
 	;;
 08)
 	sleep 0.5s
-	echo -e "\n安装aria2输入 1     安装axel输入 2\n"
+	echo -e "\naria2 -> 1    axel -> 2    annie -> 3\n"
 	read -p "[when]# 你选择的序号是：" a
 	case $a in
 	1)
@@ -213,17 +213,35 @@ case $home in
 		fi
 		mkdir -p $HOME/.config/aria2/
 		wget -O $HOME/.config/aira2/aria2.conf https://github.com/NibaZShab/NibaZShab.github.io/releases/download/09/09.conf
-		echo -e "echo \"rpc-key: 123456\"\nsleep 2s\nam start -a android.intent.action.VIEW -d http://aria2.net\naria2c --conf-path=$HOME/.config/aria2/aria2.conf" > $HOME/aria2
-		chmod 777 $HOME/aria2/
-		echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 输入 ~/aria2 开始 $colorend"
+		echo -e "echo \"rpc-key: 123456\"\nsleep 2s\nam start -a android.intent.action.VIEW -d http://aria2.net\naria2c --conf-path=$HOME/.config/aria2/aria2.conf\"" > $HOME/aria2
+		chmod 777 $HOME/aria2
+		echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 输入~/aria2 开始 $colorend"
 		home0 ; home1
 		;;
 	2)
 		sleep 0.5s
 		apt-get install -y axel
-		echo -e "下载百度云方法：前往 https://www.baidusu.com 获取下载链接，然后输入 axel -n 256 -o /sdcard/Download/文件名 下载链接" > $HOME/axelh
+		echo -e "echo \"下载百度云方法：前往 https://www.baidusu.com 获取下载链接，然后输入 axel -n 256 -o /sdcard/Download/文件名 下载链接\"" > $HOME/axelh
 		chmod 777 $HOME/axelh
 		echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 输入 ~/axelh 查看下载百度云教程 $colorend"
+		home0 ; home1
+		;;
+	3)
+		sleep 0.5s
+		apt-get install -y golang
+		apt-get install -y git
+		cd $HOME
+		git clone https://github.com/iawia002/annie.git
+		cd $HOME/annie/
+		echo -e "$colorhint 编译时间较长，请耐心等待 $colorend"
+		sleep 2s
+		GOOS=android GOARCH=arm64 go build -o annie main.go
+		mv -f annie $PREFIX/bin/
+		cd $HOME
+		rm -rf $HOME/annie
+		chmod -Rf 777 $HOME/go/
+		rm -rf $HOME/go/
+		echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 编译指令感谢 coolapk@萌系生物研究员 提供 $colorend \n $colorhint 使用annie的教程请自行百度 $colorend"
 		home0 ; home1
 		;;
 	*)
@@ -248,7 +266,7 @@ case $home in
 10)
 	echo -e "\n\n\n\n\n"
 	logo
-	sleep 0.04s
+	sleep 0.025s
 	work0 ; work1
 	;;
 11)
@@ -271,21 +289,26 @@ case $home in
 99)
 	read -p "[when]# 请输入充值的软妹币额度：" c
 	case $c in
-		[0-9]*)
-			read -p "[when]# 请输入支付密码："
-			echo -e "[when]# 正在链接服务器......"
-			sleep 1s
-			echo -e "[when]# 恭喜！已成功充入 $c 软妹币！"
-			sleep 0.2s
-			echo -e "[when]# 已升级为 svip 版！"
-			;;
-		*)
-			sleep 1s
-			echo -e "[when]# 服务器错误，你的软妹币全没啦！"
-			;;
-		esac
+	[0-9]*)
+		read -p "[when]# 请输入支付密码："
+		echo -e "[when]# 正在链接服务器......"
+		sleep 1s
+		echo -e "[when]# 恭喜！已成功充入 $c 软妹币！"
+		sleep 0.2s
+		echo -e "[when]# 已升级为 svip 版！"
+		;;
+	*)
+		sleep 1s
+		echo -e "[when]# 服务器错误，你的软妹币全没啦！"
+		;;
+	esac
 	;;
 00)
+	clear
+	cat $PREFIX/etc/motd
+	exit
+	;;
+exit)
 	clear
 	cat $PREFIX/etc/motd
 	exit
@@ -301,31 +324,31 @@ function game0(){
 cd $HOME
 echo -e "\n\n"
 echo -e "01)  小火车"
-sleep 0.04s
+sleep 0.025s
 echo -e "02)  彩虹猫"
-sleep 0.04s
+sleep 0.025s
 echo -e "03)  screenfetch"
-sleep 0.04s
+sleep 0.025s
 echo -e "04)  neofetch"
-sleep 0.04s
+sleep 0.025s
 echo -e "05)  奶牛说话"
-sleep 0.04s
+sleep 0.025s
 echo -e "06)  打开王者荣耀"
-sleep 0.04s
+sleep 0.025s
 echo -e "07)  刷流量"
-sleep 0.04s
+sleep 0.025s
 echo -e "08)  欧皇测试游戏"
-sleep 0.04s
+sleep 0.025s
 echo -e "09)  贪吃蛇"
-sleep 0.04s
+sleep 0.025s
 echo -e "10)  终端纯文本浏览器"
-sleep 0.04s
+sleep 0.025s
 echo -e "11)  音乐播放器"
-sleep 0.04s
+sleep 0.025s
 echo -e "00)  返回"
-sleep 0.04s
+sleep 0.025s
 echo -e "---------------------------------------------"
-sleep 0.04s
+sleep 0.025s
 echo -e ""
 }
 function game1(){
@@ -377,7 +400,7 @@ case $game in
 	i=0
 	while [ $((i++)) -le 999 ]
 	do
-		wget -O /dev/null https://g37.gdl.netease.com/onmyoji_setup_9.7.0.zip
+		wget -O /dev/null http://dlied5.myapp.com/myapp/1104466820/sgame/2017_com.tencent.tmgp.sgame_h8218_1.43.1.15_fc9dc4.apk
 	done
 	;;
 08)
@@ -400,17 +423,24 @@ case $game in
 	apt-get install -y elinks
 	apt-get install -y lynx
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 三个浏览器分别为 w3m, elinks, lynx  使用方法请自行百度 $colorend"
+	game0 ; game1
 	;;
 11)
-	sleep
+	sleep 0.5s
 	apt-get install -y mpv
 	echo -e "$colorhint 进度 [100%] $colorend \n $colorhint 使用方法：mpv 音乐链接"
+	game0 ; game1
 	;;
 00)
 	echo -e "\n\n\n\n\n"
 	logo
-	sleep 0.04s
+	sleep 0.025s
 	home0 ; home1
+	;;
+exit)
+	clear
+	cat $PREFIX/etc/motd
+	exit
 	;;
 *)
 	echo -e "[when]# 你正确输入了一个错误序号！"
@@ -423,19 +453,19 @@ function work0(){
 cd $HOME
 echo -e "\n\n"
 echo -e "01)  python"
-sleep 0.04s
+sleep 0.025s
 echo -e "02)  java"
-sleep 0.04s
+sleep 0.025s
 echo -e "03)  go"
-sleep 0.04s
+sleep 0.025s
 echo -e "04)  c/c++"
-sleep 0.04s
+sleep 0.025s
 echo -e "05)  php"
-sleep 0.04s
+sleep 0.025s
 echo -e "00)  返回"
-sleep 0.04s
+sleep 0.025s
 echo -e "---------------------------------------------"
-sleep 0.04s
+sleep 0.025s
 echo -e ""
 }
 function work1(){
@@ -477,8 +507,13 @@ case $work in
 00)
 	echo -e "\n\n\n\n\n"
 	logo
-	sleep 0.04s
+	sleep 0.025s
 	home0 ; home1
+	;;
+exit)
+	clear
+	cat $PREFIX/etc/motd
+	exit
 	;;
 *)
 	echo -e "[when]# 你正确输入了一个错误序号！"
@@ -488,6 +523,6 @@ case $work in
 esac
 }
 logo
-sleep 0.04s
+sleep 0.025s
 island
 exit
